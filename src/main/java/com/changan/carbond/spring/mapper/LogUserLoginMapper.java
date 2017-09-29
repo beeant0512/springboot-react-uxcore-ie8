74ff22e67868dba33b123840f773bd2eb4ec1df0
Beeant <huangxb0512@gmail.com>
2017-09-29 18:28:11 +0800

    rename package

--- src/main/java/com/changan/carbond/spring/mapper/LogUserLoginMapper.java
+++
@@ -1,9 +0,0 @@
-package com.changan.carbond.spring.mapper;
-
-import com.changan.carbond.common.IBaseDao;
-import com.changan.carbond.spring.model.LogUserLogin;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface LogUserLoginMapper extends IBaseDao<LogUserLogin> {
-}
--- src/main/java/com/changan/carbond/spring/mapper/MenuMapper.java
+++
@@ -1,9 +0,0 @@
-package com.changan.carbond.spring.mapper;
-
-import com.changan.carbond.common.IBaseDao;
-import com.changan.carbond.spring.model.Menu;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MenuMapper extends IBaseDao<Menu> {
-}
